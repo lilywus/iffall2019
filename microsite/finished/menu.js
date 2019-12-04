752a59928b467fbeffd11362e484a7a2542f302c
Lily W <lilywu256@gmail.com>
2019-12-04 08:40:30 -0600

    menu configuration

--- microsite/finished/menu.js
+++ microsite/finished/menu.js
@@ -1,9 +1,17 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.display = "block";
   document.getElementById("main").style.marginLeft = "300px";
+  var all = document.getElementsByClassName('content');
+	for (var i = 0; i < all.length; i++) {
+	  all[i].style.marginTop = '10px';
+  }
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.display = "none";
   document.getElementById("main").style.marginLeft= "0";
+  var all = document.getElementsByClassName('content');
+	for (var i = 0; i < all.length; i++) {
+	  all[i].style.marginTop = '-90px';
+  }
 }
